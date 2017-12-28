278711b8f491e0149c9f44d3dfdee10176c1209b
Nathan Horrigan <nathan_horrigan@icloud.com>
2017-12-28 19:22:41 +0000

    file setup delete

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/Nathan/Sites/ct-park/src/layouts/index.js"
+  import data from "/Users/Nathan/Sites/ct-park/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.layouts = {
+  "layout---index": preferDefault(require("/Users/Nathan/Sites/ct-park/.cache/layouts/index.js"))
+}
+
+exports.components = {
+  "component---src-pages-index-js": preferDefault(require("/Users/Nathan/Sites/ct-park/src/pages/index.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("/Users/Nathan/Sites/ct-park/.cache/json/layout-index.json"),
+  "index.json": require("/Users/Nathan/Sites/ct-park/.cache/json/index.json")
+}
