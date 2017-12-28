d82983cbd71161cde118c93a9a7008bb9eda6f2b
Nathan Horrigan <nathan_horrigan@icloud.com>
2017-12-28 19:30:26 +0000

    publish: build
    generated from commit cc426c1fe3301ff66e2c389f074d97773b53c118

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/Nathan/Sites/ct-park/src/layouts/index.js"
-  import data from "/Users/Nathan/Sites/ct-park/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
